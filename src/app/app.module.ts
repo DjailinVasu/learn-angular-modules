cd944c9a7abc689a562a2cf51b912d0abaf634f4
Mary Denysova <djailin.vasu@gmail.com>
2020-04-12 19:37:15 +0300

    Ленивая загрузка модулей

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,7 @@ import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
 import {HomePageComponent} from './home-page/home-page.component'
 import {AppRoutingModule} from './app-routing.module'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
-import {ColorDirective} from './shared/color.directive'
-import {PageNamePipe} from './shared/page-name.pipe'
-import { AboutPageModule } from './about-page/about-page.module';
+
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -23,7 +19,6 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     NgbModule,
     AppRoutingModule,
-    AboutPageModule,
     SharedModule
   ],
   providers: [],
