167e6f181e7b25846ffc985989b510cb3d7457ac
Mary Denysova <djailin.vasu@gmail.com>
2020-04-12 19:30:39 +0300

    Создание отдельного модуля

---
+++ src/app/about-page/about-page.module.ts
@@ -0,0 +1,29 @@
+
+import { AboutPageComponent } from './about-page.component';
+import { NgModule } from '@angular/core';
+import { AboutExtraPageComponent } from './about-extra-page/about-extra-page.component';
+import { SharedModule } from '../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    AboutExtraPageComponent
+  ],
+  imports: [
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'about', component: AboutPageComponent, children: [
+        {path: 'extra', component: AboutExtraPageComponent}
+      ]},
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AboutPageModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
@@ -10,21 +10,21 @@ import {AboutPageComponent} from './about-page/about-page.component'
 import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 import {ColorDirective} from './shared/color.directive'
 import {PageNamePipe} from './shared/page-name.pipe'
+import { AboutPageModule } from './about-page/about-page.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    AboutPageComponent,
-    AboutExtraPageComponent,
-    ColorDirective,
-    PageNamePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    NgbModule,
     AppRoutingModule,
-    NgbModule
+    AboutPageModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
